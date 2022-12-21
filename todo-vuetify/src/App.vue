aa646ad6a8f8aa9ed3333e5196c50c5402442f96
srimalonline <dilankasrimal23@gmail.com>
2022-12-21 15:25:45 +0530

    adding icons and title as an array

--- todo-vuetify/src/App.vue
+++ todo-vuetify/src/App.vue
@@ -1,23 +1,43 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer v-model="drawer">
-      <!--  -->
-    </v-navigation-drawer>
+    <v-navigation-drawer
+        v-model="drawer"
+        app
+      >
+        <v-list-item
+          prepend-avatar=""
+          title="Application"
+        ></v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+          <v-list-item prepend-icon="mdi-view-dashboard" title="Todo" value="home"></v-list-item>
+          <v-list-item prepend-icon="mdi-forum" title="About" value="about"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
 
     <v-app-bar>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-toolbar-title>Vuetify todo</v-toolbar-title>
     </v-app-bar>
 
     <v-main>
       <!--  -->
+      <router-vue></router-vue>
     </v-main>
   </v-app>
 </template>
 
 <script>
   export default {
-    data: () => ({ drawer: null }),
+    data: () => ({
+      drawer: null,
+      items: [
+          { title: 'Home', icon: 'mdi-home-city' },
+          { title: 'My Account', icon: 'mdi-account' },
+          { title: 'Users', icon: 'mdi-account-group-outline' },
+        ], }),
   }
 </script>
