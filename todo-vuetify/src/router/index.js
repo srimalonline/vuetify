2994fa93ccddbbace61ea7bfd0bb2f5945af47cc
srimalonline <dilankasrimal23@gmail.com>
2022-12-22 23:27:25 +0530

    codebase for vue-router index

---
+++ todo-vuetify/src/router/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../Home.vue'
+
+Vue.useAttrs(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // router level code-splitting
+    // this generates a separate chunk (about. [hash].js) for this route
+    //which is lazy-loaded when the router is visited.
+    component: () => import('../views/About.vue')
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
