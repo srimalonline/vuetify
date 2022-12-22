de431d11812a83a48e0106e88dbd98e7c65f60ca
srimalonline <dilankasrimal23@gmail.com>
2022-12-22 23:49:25 +0530

    creating dark theme button

--- todo-vuetify/src/App.vue
+++ todo-vuetify/src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <v-app id="inspire">
+  <v-app id="inspire" :theme="theme">
+
+      <v-spacer></v-spacer>
+
+
     <v-navigation-drawer
         v-model="drawer"
         app
@@ -20,12 +24,16 @@
     <v-app-bar>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Vuetify todo</v-toolbar-title>
+      <v-toolbar-title>Vuetify todo
+        <v-btn
+        :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'"
+        @click="onClick"
+      >Toggle Theme</v-btn>
+      </v-toolbar-title>
     </v-app-bar>
 
     <v-main>
-      <!--  -->
-      <router-vue></router-vue>
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
@@ -39,5 +47,14 @@
           { title: 'My Account', icon: 'mdi-account' },
           { title: 'Users', icon: 'mdi-account-group-outline' },
         ], }),
+  }
+</script>
+<script setup>
+  import { ref } from 'vue'
+
+  const theme = ref('light')
+
+  function onClick () {
+    theme.value = theme.value === 'light' ? 'dark' : 'light'
   }
 </script>
---
+++ todo-vuetify/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <img src="../assets/logo.png" alt="Vue logo">
+    <HelloWorld msg="Welcome to Your Vue.js app"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
